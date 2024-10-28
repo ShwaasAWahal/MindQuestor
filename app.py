8becccb8bc02855b8fd5440c9ea225f4a9a9cd39
ShwaasAWahal <72555602+ShwaasAWahal@users.noreply.github.com>
2024-10-28 13:25:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , url_for , request
 import mysql.connector
 
 app = Flask("__name__")
@@ -6,7 +6,20 @@ app = Flask("__name__")
 @app.route("/")
 @app.route("/home")
 def home():
+    return render_template('home.html')
+
+@app.route("/register" , methods=['POST' , 'GET'])
+def register():
+    if request.method == 'POST':
+        print(dict(request.form)["em"])
+    return render_template('register.html')
+
+
+@app.route("/login" , methods = ['GET' ,'POST'])
+def login():
     return "<h1> Hello World! </h1>"
+
+
 
 
 

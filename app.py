9f29a2fb9a6d7255c8462c273c49faee7e6d6e4b
ShwaasAWahal <72555602+ShwaasAWahal@users.noreply.github.com>
2024-11-01 18:38:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,55 @@
-from flask import Flask , render_template , url_for , request
-import mysql.connector
-
+from flask import Flask , render_template , url_for , request , redirect
+from flask_sqlalchemy import SQLAlchemy
 app = Flask("__name__")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+app.app_context().push()
+class Database(db.Model):
+    id = db.Column(db.Integer , primary_key = True)
+    user_name = db.Column(db.String(255) ,nullable = False )
+    Email = db.Column(db.String(255) ,nullable = False )
+    password = db.Column(db.String(255) ,nullable = False )
+
+    def __repr__(self):
+        return f"{self.id} - {self.Email} - {self.user_name}"
+    
+
+questions = {"What is 1 + 1" : [1 ,2 ,3 ,4] , "What is 2+2" : [3 , 5 , 6 , 8] , "what is 3 + 3" : [2 , 3 , 4 , 6]}
 
 @app.route("/")
-@app.route("/home")
+@app.route("/home" , methods = ['GET' , 'POST'])
 def home():
-    return render_template('home.html')
+    subject = ""
+    return render_template('main_web_page.html' , sub = subject)
 
-@app.route("/register" , methods=['POST' , 'GET'])
-def register():
+
+@app.route("/signin" , methods = ['POST' , 'GET'])
+def SignIn():
     if request.method == 'POST':
-        print(dict(request.form)["em"])
-    return render_template('register.html')
+        email = request.form['email']
+        password = request.form['password']
+    return render_template('signin.html')
+
+@app.route("/signup" , methods = ['GET' , 'POST'])
+def SignUp():
+    if request.method == 'POST':
+        print(request.form)
+    return render_template('signup.html')
 
 
-@app.route("/login" , methods = ['GET' ,'POST'])
-def login():
-    return "<h1> Hello World! </h1>"
+@app.route("/subjects/<sub>")
+def Subjects(sub):
+    return render_template("subject.html" , subject = sub , subC = sub.capitalize())
 
-
+@app.route("/subjects/<sub>/<level>" , methods = ['GET' , 'POST'])
+def level(sub , level):
+    if request.method == 'POST':
+        for i in request.form:
+            print(request.form[i])
+        #print(request.form)
+    return render_template("levels.html" , questions = questions , subject = sub.capitalize() , level = level)
 
 
 
--- sql.py
+++ sql.py
@@ -1,42 +1,26 @@
-import mysql.connector as c
 from flask import Flask, render_template, redirect, request, session
 ##MySQL Connection
-con=c.connect (host="localhost",user="root", passwd="Yash@12345")
-cursor=con.cursor()
-cursor.execute("create database if not exists MindQuestor_db")
-cursor.execute("use MindQuestor_db")
+'''
+def connect(con , cursor):
+    con=c.connect (host="localhost",user="root", passwd="shoyshwaas")
+    cursor=con.cursor()
+    cursor.execute("create database if not exists MindQuestor_db")
+    cursor.execute("use MindQuestor_db")
 
-##Creating Required Tables
-cursor.execute("""create table if not exists user_details(
-               ID int auto_increment primary key, 
-               USER_NAME varchar(255) not null,
-               EMAIL varchar(255) not null,
-               PASSWORD varchar(255) not null )""")
-cursor.execute("""create table if not exists results(
-               ID int auto_increment primary key,
-               USER_ID int,
-               SUBJECT varchar(255),
-               Test_1_Score int default NULL,
-               Test_2_Score int default NULL,
-               Test_3_Score int default NULL,
-               FOREIGN KEY (USER_ID) REFERENCES user_details(ID))""")
-con.commit()
-
-##Route for Signup
-@app.route("/")
-@app.route("/signup", methods = ["Get", "Post"])
-def signup():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-
-        cursor.execute("INSERT INTO user_details (user_name, email, password) VALUES (%s, %s, %s)", 
-                       (username, email, password))
-        db.commit()
-
-        session['username'] = username
-        return redirect('/subjects')
-
-    return render_template('signup.html')
+    ##Creating Required Tables
+    cursor.execute("""create table if not exists user_details(
+                ID int auto_increment primary key, 
+                USER_NAME varchar(255) not null,
+                EMAIL varchar(255) not null,
+                PASSWORD varchar(255) not null )""")
+    cursor.execute("""create table if not exists results(
+                ID int auto_increment primary key,
+                USER_ID int,
+                SUBJECT varchar(255),
+                Test_1_Score int default NULL,
+                Test_2_Score int default NULL,
+                Test_3_Score int default NULL,
+                Test_4_Score int default NULL,
+                FOREIGN KEY (USER_ID) REFERENCES user_details(ID))""")
+    con.commit()'''
 
